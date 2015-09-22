04f3f2235b9066fb2f44123a660b746b5f35e004
Kyoder2 <kyoder2@butlercc.edu>
2015-09-22 17:57:12 -0500

    Beginning States
    Beginning State scripts

---
+++ Unity_Project/Assets/Code/States/BegingState.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BegingState : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
