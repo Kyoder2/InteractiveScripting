e34ec4f448205ba14f9ea5099f850817d1e9ac1a
Kyoder2 <kyoder2@butlercc.edu>
2015-09-22 18:27:16 -0500

    Begin State Scripts
    Setting up State Scripts

---
+++ Unity_Project/Assets/Code/States/BeginState.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Assets.Code.Interfaces;
+
+namespace Assets.Code.States
+{
+	public class BeginState : IStateBase
+	{
+		public BeginState()
+		{
+			Debug.Log("Constructing BeginState");
+		}
+
+		public void StateUpdate()
+		{
+
+		}
+
+		public void ShowIt()
+		{
+			
+		}
+	}
+}
